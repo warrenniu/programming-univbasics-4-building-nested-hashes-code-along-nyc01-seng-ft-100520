f4e1b849198054b95c9edffc280df6fabe986a12
Warren Niu <warrenniu@gmail.com>
2020-09-14 15:57:13 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -6,7 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        :name => "Benvolio", :age => "17", :attitude => "worried"
+        {:name => "Benvolio", :age => "17", :attitude => "worried"},
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
    },
    :capulet => {
@@ -15,6 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+        {:name => "Steven", :age => "30", :attitude => "confused"},
+        {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
